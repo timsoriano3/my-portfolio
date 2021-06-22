0559c421fb51aa5654863687a53a7b69a9da8dd1
Peter Tim Soriano <chops10@my.yorku.ca>
2021-06-21 20:32:10 -0400

    First Blog Posts, Projects Page, First Project

--- src/components/Post.js
+++ src/components/Post.js
@@ -31,7 +31,7 @@ export default function Post() {
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">
                     Welcome to my page of blog posts!
                 </h2>
-                <div className="grid md:grid-cols-2 lg:grid-cold-3 gap-8">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
                         <article>
                         <Link to={"/post/" + post.slug.current} key={post.slug.current}>
