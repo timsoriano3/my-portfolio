0fb1f528f1c006b347f08977926411f3baf44695
Peter Tim Soriano <chops10@my.yorku.ca>
2021-07-10 20:29:22 -0400

    gallery edit 1

--- myportfolio/schemas/schema.js
+++ myportfolio/schemas/schema.js
@@ -22,6 +22,7 @@ export default createSchema({
     post,
     author,
     project,
+    fish,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
