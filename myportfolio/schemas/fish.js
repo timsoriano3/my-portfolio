61c51501d31edbc12759f2ec838b9c3dd6d31b8f
Peter Tim Soriano <chops10@my.yorku.ca>
2021-07-10 21:15:59 -0400

    gallery complete, sanity redeploy, navbar integration

--- myportfolio/schemas/fish.js
+++ myportfolio/schemas/fish.js
@@ -3,35 +3,35 @@ export default {
     title: 'Fish',
     type: 'document',
     fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-      },
-      {
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        options: {
-          source: 'title',
-          maxLength: 96,
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
         },
-      },
-      {
-        name: 'mainImage',
-        title: 'Main image',
-        type: 'image',
-        options: {
-          hotspot: true,
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title',
+              maxLength: 96,
+            },
         },
-      }
+        {
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        }
     ],
-  
+
     preview: {
-      select: {
-        title: 'title',
-        media: 'mainImage',
-      },
+        select: {
+            title: 'title',
+            media: 'mainImage',
+        },
     },
-  }
+}
   
--- myportfolio/schemas/schema.js
+++ myportfolio/schemas/schema.js
@@ -22,8 +22,8 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    fish,
     project,
+    fish,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
